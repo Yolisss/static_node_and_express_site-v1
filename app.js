138e87ba0a2277320439738d3465acc814e49682
Yolisma <yolisisabel28@gmail.com>
2023-11-28 11:14:30 -0800

    added static middleware. Rendering on browser

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const dataJson = require("./data.json");
 //returns express application
 const app = express();
 
+//#6 part B
+app.use("/static", express.static("public"));
+
 //2. set your 'view enginer' to 'pug'
 //using pug format when we render content from server to browser
 app.set("view engine", "pug");
