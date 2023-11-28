0ed691b39ca94255371cf78fdab632e9b0020245
Yolisma <yolisisabel28@gmail.com>
2023-11-28 09:45:21 -0800

    set view engine to pug and initialized first route for index

---
+++ app.js
@@ -0,0 +1,17 @@
+//1.
+const express = require("express");
+const dataJson = require("./data.json");
+
+//returns express application
+const app = express();
+
+//2. set your 'view enginer' to 'pug'
+//using pug format when we render content from server to browser
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//setting up dev server using listen method
+app.listen(3000);
