573db6df7a0901b01be7409a489758e284d2971d
Yolisma <yolisisabel28@gmail.com>
2023-11-28 20:35:23 -0800

    restructured get requests for main and project routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //1.
 const express = require("express");
-const dataJson = require("./data.json");
+const projects = require("./data.json").projects;
+const path = require("path");
 
 //returns express application
 const app = express();
@@ -13,17 +14,53 @@ app.use("/static", express.static("public"));
 //using pug format when we render content from server to browser
 app.set("view engine", "pug");
 
+//app.use(project);
+
 //#6 D setting routes; use get method on app obj
 //home page
 app.get("/", (req, res) => {
-  res.render("index");
+  //passing in index file, passing in 'projs'
+  //var since it gives us access to data.json
+  //this will then allow us to render data in index.js
+  //and begin filling out what's asking us to grab
+  res.render("index", { projects });
 });
 //about page
 app.get("/about", (req, res) => {
   res.render("about");
 });
+
+//in 'card template' video
+//mentioned something about req.query
+//req.params
+
+//route param from URL will be stored in req obj params prop
+//using a colon tells express to treat this part of the URL
+//as a var or a route param name id
+//the value for the route param from the URL will be stored in the req
+//obj params property
 app.get("/project/:id", (req, res) => {
-  res.render("project");
+  const projectId = req.params.id;
+  const project = projects.find((project) => project.id === projectId);
+  if (project) {
+    res.render("project", { projects });
+  } else {
+    res.status(404).send("Project not found");
+  }
+});
+
+//#7 A; creating a custom error obj
+app.use((req, res, next) => {
+  const err = new Error("Sorry, page not found");
+  err.status = 404;
+  next(err);
+});
+
+//#7 B global error
+app.use((err, req, res, next) => {
+  err.status = err.status || 500;
+  err.message = err.message || "Internal Server Error";
+  console.log(`Error: ${err.message}, Status: ${err.status}`);
 });
 
 //setting up dev server using listen method
